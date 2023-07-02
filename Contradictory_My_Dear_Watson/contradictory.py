91a3c6ab82b8afe57af4b08ccd0b387a6036488a
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2023-07-02 19:52:00 +0200

    Change strategy to basic CPU

--- Contradictory_My_Dear_Watson/contradictory.py
+++ Contradictory_My_Dear_Watson/contradictory.py
@@ -79,38 +79,32 @@ def build_model(
     return model
 
 
-try:
-    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
-    tf.config.experimental_connect_to_cluster(tpu)
-    tf.tpu.experimental.initialize_tpu_system(tpu)
-    strategy = tf.distribute.experimental.TPUStrategy(tpu)
-except ValueError:
-    strategy = tf.distribute.get_strategy() # for CPU and single GPU
-    print('Number of replicas:', strategy.num_replicas_in_sync)
+def main():
+    train_data = pd.read_csv("./data/train.csv")
+    test_data = pd.read_csv("./data/test.csv")
 
-train_data = pd.read_csv("./data/train.csv")
-test_data = pd.read_csv("./data/test.csv")
+    labels, frequencies = np.unique(train_data.language.values, return_counts=True)
 
-labels, frequencies = np.unique(train_data.language.values, return_counts=True)
+    plt.figure(figsize=(10,10))
+    plt.pie(frequencies, labels=labels, autopct='%1.1f%%')
+    plt.savefig("language_distribution.svg", format="svg")
+    plt.show()
 
-plt.figure(figsize=(10,10))
-plt.pie(frequencies, labels=labels, autopct='%1.1f%%')
-plt.savefig("language_distribution.svg", format="svg")
-plt.show()
+    bert_name = 'bert-base-multilingual-cased'
+    tokenizer = BertTokenizer.from_pretrained(bert_name)
 
-bert_name = 'bert-base-multilingual-cased'
-tokenizer = BertTokenizer.from_pretrained(bert_name)
-
-train_input = bert_encode(train_data.premise.values, train_data.hypothesis.values, tokenizer)
-with strategy.scope():
+    train_input = bert_encode(train_data.premise.values, train_data.hypothesis.values, tokenizer)
     model = build_model(bert_name)
-    model.summary()
 
-model.fit(train_input, train_data.label.values, epochs=2, batch_size=64, validation_split=0.3)
+    model.fit(train_input, train_data.label.values, epochs=2, batch_size=64, validation_split=0.3)
 
-test_input = bert_encode(test_data.premise.values, test_data.hypothesis.values, tokenizer)
+    test_input = bert_encode(test_data.premise.values, test_data.hypothesis.values, tokenizer)
 
-predictions = [np.argmax(i) for i in model.predict(test_input)]
+    predictions = [np.argmax(i) for i in model.predict(test_input)]
 
-output = pd.DataFrame({'prediction': predictions})
-output.to_csv('submission.csv', index=False)
+    output = pd.DataFrame({'prediction': predictions})
+    output.to_csv('submission.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
